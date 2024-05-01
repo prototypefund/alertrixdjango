eb21966a569c95e81d0544a6d4b2f45bd526e60e
Fabian Becker <info@alertrix.net>
2024-05-01 14:04:08 +0200

    feat: implement custom user model
    matrix_id has the same requirements (blank, null) as the abstract users email attribute

--- alertrix/models.py
+++ alertrix/models.py
@@ -1,2 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(
+    AbstractUser,
+):
+    username = models.TextField(
+        primary_key=True,
+    )
+    matrix_id = models.TextField(
+        unique=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return str(self.username)

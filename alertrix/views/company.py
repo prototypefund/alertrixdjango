e7a9ab5c216faaa14360c50175837214eabc443a
Fabian Becker <info@alertrix.net>
2024-05-10 00:19:17 +0200

    feat: start the company create view

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -6,3 +6,27 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
 from .. import forms
 from .. import models
+
+
+class CreateCompany(
+    PermissionRequiredMixin,
+    CreateView,
+):
+    permission_required = 'alertrix.add_company'
+    model = models.Company
+    form_class = forms.company.CompanyForm
+    template_name = 'alertrix/form.html'
+    http_method_names = [
+        'get',
+        'post',
+    ]
+
+    def get_form_kwargs(self):
+        kwargs = {
+            'user': self.request.user,
+            **super().get_form_kwargs(),
+        }
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('comp.detail', kwargs={'slug': self.object.slug})

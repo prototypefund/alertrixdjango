3fe1c08077ed9e40dd0b918e082d371482b39569
Fabian Becker <info@alertrix.net>
2024-05-10 00:10:57 +0200

    import: reverse from django.urls into views/company.py

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse
 from django.views.generic import CreateView
 from .. import forms
 from .. import models

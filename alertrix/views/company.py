8a533d3cf9c84281a2f04044db366f691d842dbd
Fabian Becker <info@alertrix.net>
2024-05-10 00:11:55 +0200

    import: Group from django.contrib.auth.models into views/company.py

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import Group
 from django.urls import reverse
 from django.views.generic import CreateView
 from .. import forms

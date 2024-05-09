48cf50cb09cc3ab1f5667175797f65d201d2a5e1
Fabian Becker <info@alertrix.net>
2024-05-10 00:27:51 +0200

    feat: implement mixin to have a standardised way to deliver context actions to templates

--- alertrix/mixins.py
+++ alertrix/mixins.py
@@ -2,6 +2,17 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse
 
 
+class ContextActionsMixin:
+    context_actions = [
+    ]
+
+    def get_context_actions(self):
+        for context_action in self.context_actions:
+            if 'name' in context_action:
+                context_action['url'] = reverse(context_action['name'])
+        return self.context_actions
+
+
 class UserIsAdminForThisObjectMixin(
     UserPassesTestMixin,
 ):

cffe5455b3caa9f014c1218cc2542f89d48e51c5
Fabian Becker <info@alertrix.net>
2024-05-09 15:53:26 +0200

    import: gettext_lazy from django.utils.translation as _

--- alertrix/models.py
+++ alertrix/models.py
@@ -2,6 +2,7 @@ import nio
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import Group
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from matrixappservice.handler import Handler as ApplicationServiceHandler
 from matrixappservice.matrix.action import MatrixAction
 from matrixappservice.models import User as MatrixUser

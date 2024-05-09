cb10e95523d14be54c502ed274fe2a870964e251
Fabian Becker <info@alertrix.net>
2024-05-10 00:15:31 +0200

    import: gettext_lazy as _ from django.utils.translation into views/company.py

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.models import Group
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
 from .. import forms
 from .. import models

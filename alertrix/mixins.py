ee2bc36d99987138c0c64519da6a329675984868
Fabian Becker <info@alertrix.net>
2024-05-10 00:01:23 +0200

    feat: implement mixin to test if the user is in the group of admins for this object

--- alertrix/mixins.py
+++ alertrix/mixins.py
@@ -1,1 +1,8 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+class UserIsAdminForThisObjectMixin(
+    UserPassesTestMixin,
+):
+    def test_func(self):
+        return self.get_object().admins in self.request.user.groups.all()

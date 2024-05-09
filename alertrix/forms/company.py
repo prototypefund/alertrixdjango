67be4445f07329e90ddd5de9457518e190dd616e
Fabian Becker <info@alertrix.net>
2024-05-09 19:11:18 +0200

    feat: start company form

--- alertrix/forms/company.py
+++ alertrix/forms/company.py
@@ -1,2 +1,17 @@
 from django import forms
 from .. import models
+
+
+class CompanyForm(
+    forms.ModelForm,
+):
+    class Meta:
+        model = models.Company
+        fields = [
+            'handler',
+            'slug',
+            'admins',
+            'matrix_room_id',
+        ]
+        widgets = {
+        }

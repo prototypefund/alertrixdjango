daa3013b67a1772b89af1ef4dd25767ef38db99b
Fabian Becker <info@alertrix.net>
2024-05-11 01:18:00 +0200

    import: reverse from django.urls into views/appservice.py

--- alertrix/views/appservice.py
+++ alertrix/views/appservice.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
 from django.views.generic import DetailView

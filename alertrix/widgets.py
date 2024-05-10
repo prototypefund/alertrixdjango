3b364e47acf0b9a2580dcd59d63eaf51357ea6b9
Fabian Becker <info@alertrix.net>
2024-05-10 15:07:09 +0200

    feat: implement widget to enter numbers with labeled recommendations

--- alertrix/widgets.py
+++ alertrix/widgets.py
@@ -1,1 +1,23 @@
 from django.forms import Widget
+
+
+class IntegerWithRecommendationsField(
+    Widget,
+):
+    type = 'number'
+    template_name = 'alertrix/widgets/integerwithrecommendationsfield.html'
+
+    def __init__(self, options: list[dict]):
+        self.options = options
+        super().__init__()
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        list_id = context['widget']['name'] + '_options'
+        context['widget']['type'] = self.type
+        context['widget']['attrs']['list'] = list_id
+        context['datalist'] = {
+            'id': list_id,
+            'options': self.options,
+        }
+        return context

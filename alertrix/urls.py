5e2daec1f040870e327983d138f5b18b04934c0f
Fabian Becker <info@alertrix.net>
2024-05-10 16:26:25 +0200

    feat: add company invite view to urlpatterns

--- alertrix/urls.py
+++ alertrix/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('comp/', include([
         path('', views.company.ListCompanies.as_view(), name='comp.list'),
         path('<slug:slug>', views.company.DetailCompany.as_view(), name='comp.detail'),
+        path('<slug:slug>/invite', views.company.InviteUser.as_view(), name='comp.invite'),
     ])),
 ]

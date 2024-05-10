4715efec0101a5f8281710dbe8c1f78527c33d6d
Fabian Becker <info@alertrix.net>
2024-05-10 14:11:56 +0200

    feat: add company detail view to urlpatterns

--- alertrix/urls.py
+++ alertrix/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path('comp/', include([
         path('', views.company.ListCompanies.as_view(), name='comp.list'),
+        path('<slug:slug>', views.company.DetailCompany.as_view(), name='comp.detail'),
     ])),
 ]

1fb4a052d51c85585e8f6f932d18b12d7f52815f
Fabian Becker <info@alertrix.net>
2024-05-10 10:36:58 +0200

    import models as mas_models from matrixappservice into views/company.py

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Group
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
+from matrixappservice import models as mas_models
 from .. import forms
 from .. import mixins
 from .. import models

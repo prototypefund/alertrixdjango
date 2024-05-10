b4b4a828a19f65819543ceff472a502e4b89f1cf
Fabian Becker <info@alertrix.net>
2024-05-10 13:58:57 +0200

    feat: add company create view to urlpatterns

--- alertrix/urls.py
+++ alertrix/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('comp/', include([
         path('', views.company.ListCompanies.as_view(), name='comp.list'),
+        path('new', views.company.CreateCompany.as_view(), name='comp.new'),
         path('<slug:slug>', views.company.DetailCompany.as_view(), name='comp.detail'),
         path('<slug:slug>/invite', views.company.InviteUser.as_view(), name='comp.invite'),
     ])),

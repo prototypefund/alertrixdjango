7f699a3a4451a5a2e8301516867ab84d97834671
Fabian Becker <info@alertrix.net>
2024-05-11 23:38:36 +0200

    import: ApplicationServiceRegistration from matrixappservice.models into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
+
+from matrixappservice.models import ApplicationServiceRegistration
 from matrixappservice.models import Homeserver
 from . import forms
 from . import models

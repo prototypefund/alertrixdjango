1fae2ba4a0a8349dcf071061ce906691a0a1d8a9
Fabian Becker <info@alertrix.net>
2024-05-11 21:09:32 +0200

    import: forms from . into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
+from . import forms

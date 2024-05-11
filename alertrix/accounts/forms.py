4579c506af3ac9e0e9dc1de3aab6ab7aa048212c
Fabian Becker <info@alertrix.net>
2024-05-11 20:55:01 +0200

    feat: implement form to create registration tokens

--- alertrix/accounts/forms.py
+++ alertrix/accounts/forms.py
@@ -2,3 +2,31 @@ from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from . import models
+
+
+class CreateRegistration(
+    forms.ModelForm,
+):
+    class Meta:
+        model = models.RegistrationToken
+        fields = [
+            'valid_for_matrix_id',
+        ]
+        widgets = {
+            'valid_for_matrix_id': forms.Textarea(
+                attrs={
+                    'rows': 1,
+                    'style': ';'.join([
+                        'resize: none',
+                    ]),
+                },
+            ),
+        }
+        labels = {
+            'valid_for_matrix_id': _('matrix id'),
+        }
+
+    submit_text = _('register')
+
+    def clean_valid_for_matrix_id(self):
+        return self.data.get('valid_for_matrix_id').strip()

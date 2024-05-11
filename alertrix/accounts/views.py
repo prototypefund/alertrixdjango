f006b30d5088e60f6ee228a37f62436cf26b5260
Fabian Becker <info@alertrix.net>
2024-05-11 21:06:24 +0200

    import: get_user_model from django.contrib.auth into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views.generic import CreateView

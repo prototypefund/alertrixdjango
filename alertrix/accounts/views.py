2cc650400f62070d21f1be7a2eefc96f9e6432f3
Fabian Becker <info@alertrix.net>
2024-05-11 21:22:08 +0200

    feat: implement CreateUser view

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -7,3 +7,43 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
 from matrixappservice.models import Homeserver
 from . import forms
+
+
+class CreateUser(
+    CreateView,
+):
+    model = get_user_model()
+    form_class = forms.CreateUserForm
+    template_name = 'alertrix/form.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.save()
+        r = super().form_valid(form)
+        group_name = settings.MATRIX_VALIDATED_GROUP_NAME
+        try:
+            group = Group.objects.get(
+                name=group_name,
+            )
+        except Group.DoesNotExist:
+            group = Group(
+                name=group_name,
+            )
+            group.save()
+        group.user_set.add(
+            self.object,
+        )
+        group.save()
+        messages.success(
+            self.request,
+            _('%(user)s has been added to %(group_name)s') % {
+                'user': self.object,
+                'group_name': group_name,
+            },
+        )
+        messages.info(
+            self.request,
+            _('please make sure your password manager works by logging in'),
+        )
+        return r

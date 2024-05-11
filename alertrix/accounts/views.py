3bdf7e56eb3ad0b12b0d4158fb530478e44e9d95
Fabian Becker <info@alertrix.net>
2024-05-11 21:12:01 +0200

    import: Homeserver from matrixappservice.models into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
+from matrixappservice.models import Homeserver
 from . import forms

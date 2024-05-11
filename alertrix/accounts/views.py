4e67e5a093c34a8573945be2a8d750e25aecb7d0
Fabian Becker <info@alertrix.net>
2024-05-11 21:06:46 +0200

    import: Group from django.contrib.auth.models into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.views.generic import CreateView

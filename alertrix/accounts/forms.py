2f90050d495f7d252fd938da4e7d51eae963dfdd
Fabian Becker <info@alertrix.net>
2024-05-11 21:17:36 +0200

    feat: implement CreateUserForm

--- alertrix/accounts/forms.py
+++ alertrix/accounts/forms.py
@@ -35,3 +35,48 @@ class CreateRegistration(
 
     def clean_valid_for_matrix_id(self):
         return self.data.get('valid_for_matrix_id').strip()
+
+
+class CreateUserForm(
+    UserCreationForm,
+):
+    token = forms.CharField(
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'token',
+            'matrix_id',
+        ]
+        widgets = {
+            'matrix_id': forms.Textarea(
+                attrs={
+                    'rows': 1,
+                    'style': ';'.join([
+                        'resize: none',
+                    ]),
+                },
+            ),
+        }
+
+    def clean_token(self):
+        try:
+            token: models.RegistrationToken = models.RegistrationToken.objects.get(
+                token=self.data['token'],
+                valid_for_matrix_id=self.data['matrix_id'],
+            )
+        except models.RegistrationToken.DoesNotExist:
+            self.add_error(
+                'token',
+                _('not a valid token for %(user_id)s') % {
+                    'user_id': self.data['matrix_id'],
+                },
+            )
+            return
+        if not token.is_valid():
+            if not self.has_error('token'):
+                self.add_error(
+                    'token',
+                    _('not a valid token'),
+                )

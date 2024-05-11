23a3d3104978c51016fc83d3a67f7e471e89d44f
Fabian Becker <info@alertrix.net>
2024-05-11 23:39:17 +0200

    import: MatrixUser from matrixappservice.models into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -8,6 +8,7 @@ from django.views.generic import CreateView
 
 from matrixappservice.models import ApplicationServiceRegistration
 from matrixappservice.models import Homeserver
+from matrixappservice.models import User as MatrixUser
 from . import forms
 from . import models
 

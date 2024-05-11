74d76a9a781fc8b373f0b50f47637fbf0c253196
Fabian Becker <info@alertrix.net>
2024-05-11 23:30:04 +0200

    import: models from . into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView
 from matrixappservice.models import Homeserver
 from . import forms
+from . import models
 
 
 class CreateUser(

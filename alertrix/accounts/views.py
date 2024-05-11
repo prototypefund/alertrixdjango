5fbaaa9c872da27b90dd355a6d35a01f595dd9df
Fabian Becker <info@alertrix.net>
2024-05-11 21:08:20 +0200

    import: gettext_lazy as _ from django.utils.translation into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -3,4 +3,5 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView

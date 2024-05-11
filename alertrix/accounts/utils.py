9d32729772b8743a1f70379ecba6e9a5d9146900
Fabian Becker <info@alertrix.net>
2024-05-11 20:35:11 +0200

    feat: implement a function to return random tokens

--- alertrix/accounts/utils.py
+++ alertrix/accounts/utils.py
@@ -2,3 +2,17 @@ import secrets
 import string
 
 from django.conf import settings
+
+
+def get_token(length: int = None):
+    if length is None:
+        try:
+            length = settings.ACCOUNTS_REGISTRATION_TOKEN_LENGTH
+        except AttributeError:
+            length = 8
+    return ''.join(
+        [
+            secrets.choice(string.ascii_lowercase + string.digits)
+            for _ in range(length)
+        ]
+    )

3482d6de17e20e28c0bc4e87e5e80505214b5ceb
Fabian Becker <info@alertrix.net>
2024-05-13 18:05:09 +0200

    import: models from . into views/matrixroom.py

--- alertrix/views/matrixroom.py
+++ alertrix/views/matrixroom.py
@@ -8,6 +8,7 @@ from django.http import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
 
 from matrixappservice import models as mas_models
+from .. import models
 
 
 class CreateMatrixRoom(

b9288f0a7d04b82263ecf1c6103092a17d92ef98
Fabian Becker <info@alertrix.net>
2024-05-13 18:17:19 +0200

    feat: implement UnitForm

--- alertrix/forms/unit.py
+++ alertrix/forms/unit.py
@@ -3,3 +3,10 @@ from django.utils.translation import gettext_lazy as _
 import matrixappservice.models
 from . import matrixroom
 from .. import models
+
+
+class UnitForm(
+    matrixroom.MatrixRoomForm,
+):
+    class Meta(matrixroom.MatrixRoomForm.Meta):
+        model = models.Unit

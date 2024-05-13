d0e98313a795c15da1e7148f0ab0cbe4150fd306
Fabian Becker <info@alertrix.net>
2024-05-13 17:56:09 +0200

    feat: implement MatrixRoomForm

--- alertrix/forms/matrixroom.py
+++ alertrix/forms/matrixroom.py
@@ -1,2 +1,67 @@
 from django.utils.translation import gettext_lazy as _
 from django import forms
+
+
+class MatrixRoomForm(
+    forms.ModelForm
+):
+    class Meta:
+        fields = [
+            'name',
+            'description',
+        ]
+        widgets = {
+            'matrix_room_id': forms.Textarea(
+                attrs={
+                    'rows': 1,
+                    'style': ';'.join([
+                        'resize: none',
+                    ]),
+                },
+            ),
+        }
+        optional = [
+            'matrix_room_id',
+        ]
+        advanced = [
+            'matrix_room_id',
+        ]
+    name = forms.CharField(
+        label=_('name'),
+        widget=forms.Textarea(
+            attrs={
+                'rows': 1,
+                'style': ';'.join([
+                    'resize: none',
+                ]),
+            },
+        ),
+    )
+    description = forms.CharField(
+        label=_('description'),
+        widget=forms.Textarea(
+            attrs={
+            },
+        ),
+        required=False,
+    )
+
+    def __init__(
+            self,
+            user,
+            data=None,
+            *args, **kwargs
+    ):
+        super().__init__(
+            data=data,
+            *args, **kwargs
+        )
+        self.user = user
+        for field_name in self.Meta.optional:
+            if field_name not in self.fields:
+                continue
+            self.fields[field_name].required = False
+        for field_name in self.Meta.advanced:
+            if field_name not in self.fields:
+                continue
+            self.fields[field_name].widget.attrs['class'] = 'advanced'

39c3983936275a38c4271193beb3cce2b431717b
Fabian Becker <info@alertrix.net>
2024-05-13 14:05:10 +0200

    prep: start CreateMatrixRoom class for matrixroom child class related create views

--- alertrix/views/matrixroom.py
+++ alertrix/views/matrixroom.py
@@ -1,3 +1,10 @@
 import nio
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
+
+
+class CreateMatrixRoom(
+):
+    """
+    Parent CreateView to create MatrixRoom objects.
+    """

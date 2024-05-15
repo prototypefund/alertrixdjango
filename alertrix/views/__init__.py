631d2c3bbe71c9ede6abe4f91303532655a5f762
Fabian Becker <info@alertrix.net>
2024-05-15 13:48:53 +0200

    feat: implement home view

--- alertrix/views/__init__.py
+++ alertrix/views/__init__.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 from . import appservice
 from . import company
 from . import unit
+
+
+def home(request):
+    return render(
+        request,
+        'alertrix/home.html',
+    )

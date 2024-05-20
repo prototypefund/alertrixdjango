73c1b45652364f066a2ef729bd33eaf7407a0957
Fabian Becker <info@alertrix.net>
2024-05-20 23:50:13 +0200

    register alertrix.User to admin interface

--- alertrix/accounts/admin.py
+++ alertrix/accounts/admin.py
@@ -7,6 +7,60 @@ from alertrix.models import User
 from . import models
 
 
+class UserCreationForm(
+    auth.UserCreationForm,
+):
+    class Meta:
+        model = User
+        fields = (
+            'matrix_id',
+        )
+
+
+class UserChangeForm(
+    auth.UserChangeForm,
+):
+    class Meta:
+        model = User
+        fields = (
+            'password',
+        )
+
+
+class AlertrixUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    forms = UserChangeForm
+    model = User
+    list_display = [
+        'matrix_id',
+    ]
+    fieldsets = (
+        (None, {"fields": ("matrix_id", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    ordering = [
+        'matrix_id',
+    ]
+
+
+admin.site.register(
+    User,
+    AlertrixUserAdmin,
+)
+
 admin.site.register(
     models.RegistrationToken,
 )

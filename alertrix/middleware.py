31f89e1e5f3b8c25c0cbd05d00533f003e2730eb
Fabian Becker <info@alertrix.net>
2024-06-20 21:50:27 +0200

    style: add empty line between 3rd party and local imports into middleware.py

--- alertrix/middleware.py
+++ alertrix/middleware.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import login
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
+
 from . import models
 
 

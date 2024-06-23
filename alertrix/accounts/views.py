007653a4f8ea0ad3defbcbce12a6d0e2ab4cc505
Fabian Becker <info@alertrix.net>
2024-06-23 20:33:21 +0200

    import: UpdateView from django.views.generic into accounts/views.py

--- alertrix/accounts/views.py
+++ alertrix/accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
+from django.views.generic import UpdateView
 from django.views.generic import CreateView
 
 from matrixappservice.models import ApplicationServiceRegistration

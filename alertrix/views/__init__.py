3883bdd17644eb23f218b0352a35eab5af1b6864
Fabian Becker <info@alertrix.net>
2024-07-16 17:06:07 +0200

    use MainUserKey to return main user account of the main application service instead of using the sender_localpart account of the main application service

--- alertrix/views/__init__.py
+++ alertrix/views/__init__.py
@@ -10,12 +10,23 @@ from .. import models
 
 
 def home(request):
+    main_user = None
+    try:
+        main_user = models.MainApplicationServiceKey.objects.get(id=1).service.mainuserkey.user
+    except models.MainApplicationServiceKey.DoesNotExist:
+        messages.error(
+            request,
+            _('no main application service has been set'),
+        )
+    except matrixappservice.models.ApplicationServiceRegistration.mainuserkey.RelatedObjectDoesNotExist:
+        messages.error(
+            request,
+            _('no main user set for the main application service'),
+        )
     return render(
         request,
         'alertrix/home.html',
         context={
-            'main_user': models.MainApplicationServiceKey.objects.get(
-                id=1,
-            ).service.get_user(),
+            'main_user': main_user,
         },
     )

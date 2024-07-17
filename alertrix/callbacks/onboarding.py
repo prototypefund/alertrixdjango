b0e52daba136fb8b063ccc17ca076ecb6439a5cb
Fabian Becker <info@alertrix.net>
2024-07-17 16:24:48 +0200

    Create a DirectMessage instance if the user has not created a direct message room with this user yet, otherwise redirect to that other room

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -15,3 +15,35 @@ async def on_room_invite(
         await client.join(
             room.room_id,
         )
+    if event.content.get('is_direct', False):
+        try:
+            # Check if there already is a direct message room for this combination of users…
+            dm = await models.DirectMessage.objects.aget(
+                responsible_user__user_id=client.user_id,
+                with_user=event.sender,
+            )
+            # … direct the user to that room …
+            await client.room_send(
+                room_id=room.room_id,
+                event_type='m.room.message',
+                body={
+                    'msgtype': 'm.text',
+                    'body': 'https://matrix.to/#/%(room_id)s' % {
+                        'room_id': dm.matrix_room_id,
+                    },
+                }
+            )
+            # … and leave this room.
+            await client.room_leave(
+                room.room_id,
+            )
+        except models.DirectMessage.DoesNotExist:
+            await models.DirectMessage.objects.acreate(
+                responsible_user=await mas_models.User.objects.aget(
+                    user_id=client.user_id,
+                ),
+                matrix_room_id=room.room_id,
+                with_user=await models.User.objects.aget(
+                    matrix_id=event.sender,
+                ),
+            )

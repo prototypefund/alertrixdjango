3357f4dff0a01f2fa1c0668508285b7b972e1b30
Fabian Becker <info@alertrix.net>
2024-07-17 15:11:56 +0200

    Revert "import: HttpResponseRedirect from django.http into views/company.py"
    This reverts commit 7438ced5adc3e667b59456029153731647831144.

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -10,7 +10,6 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.models import Group
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView

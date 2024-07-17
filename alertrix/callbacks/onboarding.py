e76aa4bfe832344b546bf8675d20d74448bbbbdd
Fabian Becker <info@alertrix.net>
2024-07-17 16:21:47 +0200

    automatically join all rooms that we are invited to

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -2,3 +2,16 @@ import nio
 from matrixappservice import MatrixClient
 
 from .. import models
+
+
+async def on_room_invite(
+        client: MatrixClient,
+        room: nio.MatrixRoom,
+        event: nio.InviteMemberEvent,
+):
+    if event.state_key != client.user_id:
+        return
+    if event.membership == 'invite':
+        await client.join(
+            room.room_id,
+        )
--- alertrix/matrix_callbacks.py
+++ alertrix/matrix_callbacks.py
@@ -3,4 +3,5 @@ import nio
 from . import callbacks
 
 matrix_callbacks = [
+    (callbacks.onboarding.on_room_invite, nio.InviteMemberEvent),
 ]

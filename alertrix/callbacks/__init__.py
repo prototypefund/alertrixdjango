2323891bd52e990325a1d32392e850035b2fee73
Fabian Becker <info@alertrix.net>
2024-07-18 16:54:25 +0200

    import: MatrixClient from matrixappservice into alertrix/callbacks/__init__.py

--- alertrix/callbacks/__init__.py
+++ alertrix/callbacks/__init__.py
@@ -4,6 +4,7 @@ import re
 import traceback
 from dataclasses import dataclass
 import nio
+from matrixappservice import MatrixClient
 from . import alertrix
 from . import directmessage
 from . import encryption

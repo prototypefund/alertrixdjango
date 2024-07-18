5fccf3d0432ba816e4d553b2e386083bfe83b6e5
Fabian Becker <info@alertrix.net>
2024-07-18 13:48:50 +0200

    Revert "import: urlencode from django.utils.http into models.py"
    This reverts commit cf3cc834bb29477d28fa313b8fd4b9b9af934ad5.

--- alertrix/models.py
+++ alertrix/models.py
@@ -13,7 +13,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
 from django.utils import timezone
-from django.utils.http import urlencode
 
 from matrixappservice import exceptions as exc
 from matrixappservice.models import ApplicationServiceRegistration

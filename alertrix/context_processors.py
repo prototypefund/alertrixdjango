fcecd156784b1db0e9a7f857db2ac27a73f151db
Fabian Becker <info@alertrix.net>
2024-07-18 13:00:15 +0200

    pass the emoticon strings from settings to templates

--- alertrix/context_processors.py
+++ alertrix/context_processors.py
@@ -5,3 +5,10 @@ def service_name(request):
     return {
         'service_name': settings.SERVICE_NAME,
     }
+
+
+def emoticons(request):
+    return {
+        'emoticon_company': settings.ALERTRIX_COMPANY_EMOTICON,
+        'emoticon_unit': settings.ALERTRIX_UNIT_EMOTICON,
+    }

83819c93596be4dbb71a39e758379d24bd3712d0
Fabian Becker <info@alertrix.net>
2024-07-18 09:15:28 +0200

    verify all devices of any user who joins a room

--- alertrix/callbacks/encryption.py
+++ alertrix/callbacks/encryption.py
@@ -1,3 +1,15 @@
 import nio
 from asgiref.sync import sync_to_async
 from matrixappservice import MatrixClient
+
+
+async def verify_all_devices(
+        client: MatrixClient,
+        room: nio.MatrixRoom,
+        event: nio.RoomMemberEvent,
+):
+    if event.membership == 'join':
+        for device_id, device in client.device_store[event.sender].items():
+            await sync_to_async(client.verify_device)(
+                device,
+            )
--- alertrix/matrix_callbacks.py
+++ alertrix/matrix_callbacks.py
@@ -4,6 +4,7 @@ from . import callbacks
 
 matrix_callbacks = [
     (callbacks.onboarding.ensure_encryption, nio.RoomMessage),
+    (callbacks.encryption.verify_all_devices, nio.RoomMemberEvent),
     (callbacks.onboarding.on_room_invite, nio.InviteMemberEvent),
     (callbacks.onboarding.add_widget_to_chat, nio.RoomMessageText),
 ]

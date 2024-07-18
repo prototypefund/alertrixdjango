9cb4dc8824af0eb7f22a02aa544ac647cc8bc6dd
Fabian Becker <info@alertrix.net>
2024-07-18 12:54:26 +0200

    Revert "import: get_user_model from django.contrib.auth into views/__init__.py"
    This reverts commit 7119e16e8b39acaa03fa85ce55702308ff1264c9.

--- alertrix/views/__init__.py
+++ alertrix/views/__init__.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
-from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _

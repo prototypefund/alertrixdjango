94d2837ce5528812bfc32d816bfefcc6894e31a4
Fabian Becker <info@alertrix.net>
2024-07-21 02:17:58 +0200

    import: v1 from alertrix.events into alertrix/callbacks/alertrix/v1/emergency.py

--- alertrix/callbacks/alertrix/v1/emergency.py
+++ alertrix/callbacks/alertrix/v1/emergency.py
@@ -4,6 +4,7 @@ import nio
 from asgiref.sync import sync_to_async
 from matrixappservice import MatrixClient
 from matrixappservice.models import Event
+from alertrix.events import v1
 
 
 callbacks = (

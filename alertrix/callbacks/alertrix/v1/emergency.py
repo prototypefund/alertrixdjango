9ae5be9d221f7eded15c97028d9496e20e8e14b5
Fabian Becker <info@alertrix.net>
2024-07-21 14:15:40 +0200

    do not iterate over every unit to forward alerts to prevent sending them multiple times to the same user

--- alertrix/callbacks/alertrix/v1/emergency.py
+++ alertrix/callbacks/alertrix/v1/emergency.py
@@ -19,42 +19,46 @@ async def alert_callback(
         return
     alert_info = event.source['content'][attribute]
     alert = v1.Alert(**alert_info)
-    for unit in alert.units:
-        member_ids = Event.objects.filter(
-            type='m.room.member',
-            content__membership='join',
-            room__room_id=unit,
-        ).values_list('state_key', flat=True)
-        our_direct_messages = Event.objects.filter(
-            state_key=client.user_id,
-            unsigned__prev_content__is_direct=True,
+    member_ids = Event.objects.filter(
+        type='m.room.member',
+        content__membership='join',
+        room__room_id__in=alert.units,
+    ).values_list(
+        'state_key',
+        flat=True,
+    ).distinct(
+    )
+    our_direct_messages = Event.objects.filter(
+        state_key=client.user_id,
+        unsigned__prev_content__is_direct=True,
+        content__membership='join',
+    ).values_list(
+        'room__room_id',
+        flat=True,
+    )
+    async for member in member_ids:
+        if member == client.user_id:
+            continue
+        # Send alerts to direct messages for now
+        their_direct_messages = Event.objects.filter(
+            state_key=member,
             content__membership='join',
         ).values_list(
             'room__room_id',
             flat=True,
         )
-        async for member in member_ids:
-            if member == client.user_id:
-                continue
-            # Send alerts to direct messages for now
-            their_direct_messages = Event.objects.filter(
-                state_key=member,
-                content__membership='join',
-            ).values_list(
-                'room__room_id',
-                flat=True,
+        room_id = await our_direct_messages.intersection(their_direct_messages).aget()
+        if room_id is None:
+            continue
+        try:
+            await client.room_send(
+                room_id=room_id,
+                message_type='m.room.message',
+                content=event.source['content'],
+                ignore_unverified_devices=True,
             )
-            room_id = await our_direct_messages.intersection(their_direct_messages).aget()
-            if room_id is None:
-                continue
-            try:
-                await client.room_send(
-                    room_id=room_id,
-                    message_type='m.room.message',
-                    content=event.source['content'],
-                )
-            except nio.LocalProtocolError as e:
-                logging.error(''.join(traceback.format_exception(e)))
+        except nio.LocalProtocolError as e:
+            logging.error(''.join(traceback.format_exception(e)))
 
 
 callbacks = (

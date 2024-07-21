8566c8dfb770b0f172016e889f1f2e112c8afa26
Fabian Becker <info@alertrix.net>
2024-07-21 02:28:58 +0200

    feat: forward alarms to the recipients
    alerts are sent to the application service controlled account of a company and include a list of room ids, which correspond to the room ids of units. The account lists the members of these rooms and forwards the alert to them. Forwarding uses the direct message between the user and the application service account for now. This should become a separate alarm channel room later, to keep the direct message for configuration purposes only

--- alertrix/callbacks/alertrix/v1/emergency.py
+++ alertrix/callbacks/alertrix/v1/emergency.py
@@ -9,5 +9,54 @@ from matrixappservice.models import Event
 from alertrix.events import v1
 
 
+async def alert_callback(
+        client: MatrixClient,
+        room: nio.MatrixRoom,
+        event: nio.RoomMessageText,
+        attribute: str,
+):
+    if event.sender == client.user_id:
+        return
+    alert_info = event.source['content'][attribute]
+    alert = v1.Alert(**alert_info)
+    for unit in alert.units:
+        member_ids = Event.objects.filter(
+            type='m.room.member',
+            content__membership='join',
+            room__room_id=unit,
+        ).values_list('state_key', flat=True)
+        our_direct_messages = Event.objects.filter(
+            state_key=client.user_id,
+            unsigned__prev_content__is_direct=True,
+            content__membership='join',
+        ).values_list(
+            'room__room_id',
+            flat=True,
+        )
+        async for member in member_ids:
+            if member == client.user_id:
+                continue
+            # Send alerts to direct messages for now
+            their_direct_messages = Event.objects.filter(
+                state_key=member,
+                content__membership='join',
+            ).values_list(
+                'room__room_id',
+                flat=True,
+            )
+            room_id = await our_direct_messages.intersection(their_direct_messages).aget()
+            if room_id is None:
+                continue
+            try:
+                await client.room_send(
+                    room_id=room_id,
+                    message_type='m.room.message',
+                    content=event.source['content'],
+                )
+            except nio.LocalProtocolError as e:
+                logging.error(''.join(traceback.format_exception(e)))
+
+
 callbacks = (
+    ('alert', alert_callback),
 )

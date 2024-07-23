06be07ce5084c7fa8218390e2083a723ca9582d3
Fabian Becker <info@alertrix.net>
2024-07-23 16:25:51 +0200

    define a queryset that returns direct messages

--- alertrix/querysets.py
+++ alertrix/querysets.py
@@ -1,3 +1,19 @@
 from django.conf import settings
 from django.db.models import Q
 from matrixappservice.models import *
+
+
+direct_messages = Room.objects.filter(
+    room_id__in=Event.objects.filter(
+        Q(
+            content__membership='invite',
+            content__is_direct=True,
+        ) | Q(
+            content__membership='join',
+            unsigned__prev_content__is_direct=True,
+        ),
+    ).values_list(
+        'room__room_id',
+        flat=True,
+    ),
+)

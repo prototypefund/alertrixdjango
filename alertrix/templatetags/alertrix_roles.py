d571f9f9405d0b105ee339dfd9e7b6664e1faf73
Fabian Becker <info@alertrix.net>
2024-07-31 16:26:27 +0200

    implement filter to check if an object is interpreted as unit

--- alertrix/templatetags/alertrix_roles.py
+++ alertrix/templatetags/alertrix_roles.py
@@ -9,3 +9,8 @@ register = template.Library()
 @register.filter
 def is_company(room):
     return room in querysets.companies
+
+
+@register.filter
+def is_unit(room):
+    return room in querysets.units

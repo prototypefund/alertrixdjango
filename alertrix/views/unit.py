8be60861e20ca3ff3f94014ee6578e8a14990e94
Fabian Becker <info@alertrix.net>
2024-07-31 14:17:05 +0200

    import: settings from django.conf into alertrix/views/unit.py

--- alertrix/views/unit.py
+++ alertrix/views/unit.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from asgiref.sync import sync_to_async
+from django.conf import settings
 from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import DetailView

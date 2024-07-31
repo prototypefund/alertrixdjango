b520a8c9dbaf9d777fae2b5212935795b55b335b
Fabian Becker <info@alertrix.net>
2024-07-31 17:51:48 +0200

    Revert "implement filter to check if an object is interpreted as unit"
    This reverts commit d571f9f9405d0b105ee339dfd9e7b6664e1faf73.
    
    templatetags do not use the live database

--- alertrix/templatetags/alertrix_roles.py
+++ alertrix/templatetags/alertrix_roles.py
@@ -9,8 +9,3 @@ register = template.Library()
 @register.filter
 def is_company(room):
     return room in querysets.companies
-
-
-@register.filter
-def is_unit(room):
-    return room in querysets.units

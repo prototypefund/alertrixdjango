af618de5fe5045c71c762567ccc123d315e6834d
Fabian Becker <info@alertrix.net>
2024-07-31 11:23:59 +0200

    remove Company from alertrix admin panel

--- alertrix/admin.py
+++ alertrix/admin.py
@@ -3,23 +3,12 @@ from django.contrib import admin
 from . import models
 
 
-class CompanyAdmin(admin.ModelAdmin):
-    readonly_fields = [
-        'slug',
-    ]
-
-
 admin.site.register(
     models.MainApplicationServiceKey,
 )
 
 admin.site.register(
     models.DirectMessage,
-)
-
-admin.site.register(
-    models.Company,
-    CompanyAdmin,
 )
 
 admin.site.register(

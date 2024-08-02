3314ed7f48d695266b6eba96b66196cbce58a565
Fabian Becker <info@alertrix.net>
2024-08-02 22:20:18 +0200

    Revert "import: slugify from django.utils.text into alertrix/callbacks/onboarding.py"
    This reverts commit 371ad7f598a42f92af3f353e3e8e5611ac64fc5d.

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -6,7 +6,6 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.utils.http import urlencode
-from django.utils.text import slugify
 from django.utils.translation import gettext as _
 from matrixappservice import MatrixClient
 from matrixappservice import models as mas_models

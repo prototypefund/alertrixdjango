f4352fd09c2c3d8b0643c7fed604a4f1f34756d3
Fabian Becker <info@alertrix.net>
2024-08-02 20:34:58 +0200

    import: loader from django.template into alertrix/callbacks/account/__init__.py

--- alertrix/callbacks/account/__init__.py
+++ alertrix/callbacks/account/__init__.py
@@ -4,3 +4,4 @@ import nio
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
 from matrixappservice import MatrixClient
+from django.template import loader

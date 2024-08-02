171e6c1e868c4cdf97db6fb08603e2310f18d696
Fabian Becker <info@alertrix.net>
2024-08-02 21:44:33 +0200

    import: gettext as _ from django.utils.translation into alertrix/callbacks/account/__init__.py

--- alertrix/callbacks/account/__init__.py
+++ alertrix/callbacks/account/__init__.py
@@ -4,6 +4,7 @@ import nio
 from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
+from django.utils.translation import gettext as _
 from matrixappservice import MatrixClient
 from django.template import loader
 

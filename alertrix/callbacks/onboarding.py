7d73914e6535ba5014ee72d094df29cf836e4da7
Fabian Becker <info@alertrix.net>
2024-08-02 22:20:34 +0200

    Revert "import: urlencode from django.utils.http into alertrix/callbacks/onboarding.py"
    This reverts commit 90420ec6982b086866e4399c18c1884b16da8ae0.

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -5,7 +5,6 @@ import nio
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from django.utils.http import urlencode
 from django.utils.translation import gettext as _
 from matrixappservice import MatrixClient
 from matrixappservice import models as mas_models

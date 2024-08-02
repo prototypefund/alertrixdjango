121bb8706ee1b874dab091aaa1c63ac258283156
Fabian Becker <info@alertrix.net>
2024-08-02 22:19:40 +0200

    Revert "import: models from .. into alertrix/callbacks/onboarding.py"
    This reverts commit 7d824f5bb15a4205ada030c562002262fd71f53c.

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -11,7 +11,6 @@ from django.utils.translation import gettext as _
 from matrixappservice import MatrixClient
 from matrixappservice import models as mas_models
 
-from .. import models
 from .. import querysets
 
 

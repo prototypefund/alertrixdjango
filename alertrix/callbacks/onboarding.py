e9c6a7cb1f3aa77bd0ba90b91199b400c4b47372
Fabian Becker <info@alertrix.net>
2024-08-02 22:21:15 +0200

    Revert "import: timezone from django.utils into alertrix/callbacks/onboarding.py"
    This reverts commit e634a9589d81f0e4ccd3fa86eafb50d2d25b7bf5.

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -4,7 +4,6 @@ import secrets
 import nio
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 from django.utils.translation import gettext as _
 from matrixappservice import MatrixClient
 from matrixappservice import models as mas_models

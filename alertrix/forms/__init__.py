b84498553349ad179f4cd6b4b540298244348fe1
Fabian Becker <info@alertrix.net>
2024-08-03 18:34:13 +0200

    implement form for Widget model

--- alertrix/forms/__init__.py
+++ alertrix/forms/__init__.py
@@ -4,3 +4,21 @@ from django.utils.translation import gettext as _
 from . import company
 from . import unit
 from .. import models
+
+
+class WidgetLoginForm(
+    forms.ModelForm,
+):
+    class Meta:
+        model = models.Widget
+        fields = [
+            'activation_secret',
+        ]
+
+    def clean_activation_secret(self):
+        if self.instance.activation_secret and self.data.get('activation_secret') != self.instance.activation_secret:
+            self.add_error(
+                'activation_secret',
+                _('invalid activation secret')
+            )
+        return self.data.get('activation_secret')

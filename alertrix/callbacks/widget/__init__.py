8f663227b9a7d67bbef5f15ddabd50bae58f21f8
Fabian Becker <info@alertrix.net>
2024-08-03 18:48:46 +0200

    import: slugify from django.utils.text into alertrix/callbacks/widget/__init__.py

--- alertrix/callbacks/widget/__init__.py
+++ alertrix/callbacks/widget/__init__.py
@@ -5,3 +5,4 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.utils.http import urlencode
+from django.utils.text import slugify

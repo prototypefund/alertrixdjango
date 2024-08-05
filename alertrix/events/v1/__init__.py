31a429dadb90fb5d326c5515a974eba3fe448657
Fabian Becker <info@alertrix.net>
2024-08-05 13:58:23 +0200

    implement a dataclass to represent an alarm

--- alertrix/events/v1/__init__.py
+++ alertrix/events/v1/__init__.py
@@ -1,1 +1,11 @@
 from dataclasses import dataclass
+
+
+@dataclass
+class Alert(
+):
+    code: str  # alarm keyword
+    description: str = None
+    units: list[str] = None  # list of the units matrix room ids
+    location: tuple[float, float] = (None, None)  # latitude and longitude
+    address: str = None

d821bf96198507a2e9a6e37c9dc6484624a48add
Fabian Becker <info@alertrix.net>
2024-08-05 14:27:37 +0200

    implement the logic of the start command

--- alertrix/callbacks/start/__init__.py
+++ alertrix/callbacks/start/__init__.py
@@ -13,4 +13,21 @@ async def start(
         event: nio.RoomMessageText,
         args: argparse.Namespace,
 ):
+    # Register the user if they did not do so earlier
+    if not await get_user_model().objects.filter(
+        matrix_id=event.sender,
+    ).aexists():
+        await account.create(
+            client,
+            room,
+            event,
+            args,
+        )
+    # Set up graphical interface/widget for them
+    await widget.add_widget_to_chat(
+        client,
+        room,
+        event,
+        args,
+    )
     return

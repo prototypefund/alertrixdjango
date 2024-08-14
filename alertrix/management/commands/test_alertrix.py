5d08a369093edf01f4549891bb0b923b02a16be8
Fabian Becker <info@alertrix.net>
2024-08-14 13:05:46 +0200

    implement test_alertrix command

--- alertrix/management/commands/test_alertrix.py
+++ alertrix/management/commands/test_alertrix.py
@@ -5,3 +5,23 @@ import unittest
 from django.core.management.base import BaseCommand
 
 import alertrix
+
+
+class Command(BaseCommand):
+    help = "Test the alertrix app on a live system (make sure to create an isolated environment)"
+
+    def handle(self, *args, **options):
+        package = alertrix
+        prefix = package.__name__ + "."
+        test_modules = []
+        for importer, modname, ispkg in pkgutil.iter_modules(package.__path__, prefix):
+            short_modname = modname.removeprefix(prefix)
+            if not re.match('test*', short_modname):
+                continue
+            test_modules.append(short_modname)
+            __import__(modname, fromlist=[])
+        unittest.main(
+            module=alertrix,
+            argv=['unittest'] + test_modules,
+            exit=False,
+        )

4439e8ad9899c13964337aaa0de9d9bea81c2329
Fabian Becker <info@alertrix.net>
2024-08-19 15:52:08 +0200

    implement abstract class for TestCases to inherit from if they need an application service

--- alertrix/test.py
+++ alertrix/test.py
@@ -1,3 +1,36 @@
 import abc
 from django.contrib.auth.models import Group
 from matrixappservice import models
+
+
+class AppserviceSetup(
+    abc.ABC,
+):
+    def setUp(self):
+        self.homeserver, new = models.Homeserver.objects.get_or_create(
+            server_name='synapse.localhost',
+            url='http://matrix.synapse.localhost',
+        )
+        if new:
+            self.homeserver.save()
+        admins, new = Group.objects.get_or_create(
+            name='admins',
+        )
+        if new:
+            admins.save()
+        self.app_service, new = models.ApplicationServiceRegistration.objects.get_or_create(
+            homeserver=self.homeserver,
+            id_homeserver='alertrix',
+            as_token='<as_token>',
+            hs_token='<hs_token>',
+            admins=admins,
+            rate_limited=False,
+        )
+        if new:
+            self.app_service.save()
+        models.Namespace.objects.get_or_create(
+            app_service=self.app_service,
+            scope=models.Namespace.ScopeChoices.users,
+            exclusive=True,
+            regex='@alertrix_*',
+        )

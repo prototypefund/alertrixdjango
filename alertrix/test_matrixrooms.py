c354037f2c4e7d7156d49511bc0a6dacc0242125
Fabian Becker <info@alertrix.net>
2024-08-27 13:13:51 +0200

    style: add empty line between 3rd party and local imports into alertrix/test_matrixrooms.py

--- alertrix/test_matrixrooms.py
+++ alertrix/test_matrixrooms.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from django.test import AsyncClient
 from django.urls import reverse
 from matrixappservice import models as mas_models
+
 from . import models
 from .test import AppserviceSetup
 

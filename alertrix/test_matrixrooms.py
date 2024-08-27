aa76b186d9d7cc84c728552fbe2c5d4a2f33b851
Fabian Becker <info@alertrix.net>
2024-08-27 13:20:04 +0200

    test: implement a test to verify that creating a company works
    can be expanded to also test the creation of units

--- alertrix/test_matrixrooms.py
+++ alertrix/test_matrixrooms.py
@@ -17,3 +17,77 @@ class MatrixRoomTest(
     """
     Test managing companies and units
     """
+
+    async def test_create_organisation_and_unit(self):
+        user, new = await get_user_model().objects.aget_or_create(
+            matrix_id='@alertrix_OrganisationTest_test_create_organisation_and_unit:synapse.localhost',
+        )
+        # Create a Matrix-Account for the test user in the scope of the application service
+        mx_user, new = await mas_models.User.objects.aget_or_create(
+            user_id=user.matrix_id,
+            app_service=self.app_service,
+        )
+        mx_client = await mx_user.aget_client()
+        client = AsyncClient()
+        await client.aforce_login(user)
+        # Do not allow every user to access the company creation form
+        resp = await client.get(
+            reverse('comp.new'),
+        )
+        self.assertEqual(
+            resp.status_code,
+            403,
+        )
+        await sync_to_async(self.validated_matrix_id_group.user_set.add)(user)
+        # Allow access to the company creation form now
+        resp = await client.get(
+            reverse('comp.new'),
+        )
+        self.assertEqual(
+            resp.status_code,
+            200,
+        )
+        company_initial_data = {
+            'name': 'OrganisationTest test create organisation and unit Company',
+            'description': 'Hello World!',
+            'application_service': self.app_service.pk,
+        }
+        resp = await client.post(
+            reverse('comp.new'),
+            data=company_initial_data,
+        )
+        self.assertEqual(
+            resp.status_code,
+            302,
+        )
+        await mx_client.sync_n(
+            n=1,
+        )
+        # since we have the special case of managing the users matrix account using the application service, they
+        # already joined the room
+        self.assertEqual(
+            (
+                await models.Company.objects.aget(
+                    room_id__in=mas_models.Event.objects.filter(
+                        type='m.room.name',
+                        content__name=company_initial_data['name'],
+                    ).values_list(
+                        'room__room_id',
+                        flat=True,
+                    ),
+                )
+            ).room_id,
+            (
+                await mas_models.Room.objects.aget(
+                    room_id__in=mas_models.Event.objects.filter(
+                        type='m.room.member',
+                        state_key=user.matrix_id,
+                        content__membership='join',
+                    ).values_list(
+                        'room__room_id',
+                        flat=True,
+                    ),
+                )
+            ).room_id,
+            'User has not received an invite to a room with the correct name',
+        )

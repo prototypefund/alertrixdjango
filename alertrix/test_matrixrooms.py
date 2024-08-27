25d6c160720fc272d48870eabc9418d146e75466
Fabian Becker <info@alertrix.net>
2024-08-27 13:11:05 +0200

    import: reverse from django.urls into alertrix/test_matrixrooms.py

--- alertrix/test_matrixrooms.py
+++ alertrix/test_matrixrooms.py
@@ -2,6 +2,7 @@ from unittest import IsolatedAsyncioTestCase
 from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
 from django.test import AsyncClient
+from django.urls import reverse
 from .test import AppserviceSetup
 
 

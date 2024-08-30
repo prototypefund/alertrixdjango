e048d1e70eb65cd31c707b546e04b3cc46dd9e58
Fabian Becker <info@alertrix.net>
2024-08-30 22:47:35 +0200

    import: alert from ..forms.emergency into alertrix/views/emergency.py

--- alertrix/views/emergency.py
+++ alertrix/views/emergency.py
@@ -3,3 +3,4 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import FormView
 from matrixappservice import models as mas_models
 from alertrix import models
+from ..forms.emergency import alert

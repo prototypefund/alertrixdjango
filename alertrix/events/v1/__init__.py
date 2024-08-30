26fb26a4923ee7ecd581acf4999abe3a3c28a953
Fabian Becker <info@alertrix.net>
2024-08-30 22:09:44 +0200

    converting an Alert to a string now returns a matrix message content compatible json string

--- alertrix/events/v1/__init__.py
+++ alertrix/events/v1/__init__.py
@@ -12,3 +12,18 @@ class Alert(
     units: list[str] = None  # list of the units matrix room ids
     location: tuple[float, float] = (None, None)  # latitude and longitude
     address: str = None
+
+    def __str__(self):
+        res = loader.render_to_string(
+            'alertrix/cli/emergency/alert.json',
+            {
+                'alert': self,
+                'alert_html': json.dumps(loader.render_to_string(
+                    'alertrix/emergency/alert.html',
+                    {
+                        'alert': self,
+                    },
+                )),
+            },
+        )
+        return res

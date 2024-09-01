ec4b1612d3b956cad140b8eaa3791855325cc8c5
Fabian Becker <info@alertrix.net>
2024-09-01 23:20:52 +0200

    import: Q from django.db.models into alertrix/models.py

--- alertrix/models.py
+++ alertrix/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import Group
 from django.db import models
+from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
 from matrixappservice import exceptions as exc

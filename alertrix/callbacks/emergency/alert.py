c10e379790cec300980420a8fdc1515116a1f1c4
Fabian Becker <info@alertrix.net>
2024-09-01 13:49:35 +0200

    import: HttpRequest from django.http into alertrix/callbacks/emergency/alert.py

--- alertrix/callbacks/emergency/alert.py
+++ alertrix/callbacks/emergency/alert.py
@@ -7,3 +7,4 @@ from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
 from django.contrib.messages.constants import DEFAULT_TAGS as DEFAULT_MESSAGE_TAGS
 from django.contrib.messages.storage import default_storage
+from django.http import HttpRequest

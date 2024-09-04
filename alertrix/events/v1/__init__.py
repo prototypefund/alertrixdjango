0f956f8369d7e0db36d65af5fa649a78d00e1cbb
Fabian Becker <info@alertrix.net>
2024-09-04 15:08:20 +0200

    implement base class for matrix events

--- alertrix/events/v1/__init__.py
+++ alertrix/events/v1/__init__.py
@@ -5,6 +5,43 @@ from django.conf import settings
 from django.template import loader
 
 
+class Event(
+):
+    version: str = settings.ALERTRIX_VERSION
+    type: str = None
+    content: dict = None
+    state_key: dict = None
+
+    def get_type(self):
+        return self.type
+
+    def get_content(self):
+        return {
+            'version': self.version,
+            **(self.content or dict()),
+        }
+
+    def get_state_key(self):
+        return self.state_key
+
+    def get_matrix_data(self, **kwargs):
+        return {
+            k: v
+            for k, v in {
+                'type': self.get_type(),
+                'content': self.get_content(),
+                'state_key': self.get_state_key(),
+                **kwargs,
+            }.items()
+            if None not in [k, v]
+        }
+
+    def __str__(self):
+        return json.dumps(
+            self.get_matrix_data(),
+        )
+
+
 @dataclass
 class Alert(
 ):

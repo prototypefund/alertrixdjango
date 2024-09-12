3efd73c0ff5ae8f0e533430481d174a828481a9d
Fabian Becker <info@alertrix.net>
2024-09-12 14:45:54 +0200

    add `id` field to WidgetLoginForm

--- alertrix/forms/__init__.py
+++ alertrix/forms/__init__.py
@@ -12,8 +12,18 @@ class WidgetLoginForm(
     class Meta:
         model = models.Widget
         fields = [
+            'id',
             'activation_secret',
         ]
+        widgets = {
+            'id': forms.HiddenInput(
+            ),
+        }
+
+    def clean_id(self):
+        if not models.Widget.objects.filter(id=self.data.get('id')).exists():
+            return _('widget does not exist')
+        return self.data.get('id')
 
     def clean_activation_secret(self):
         if self.instance.activation_secret and self.data.get('activation_secret') != self.instance.activation_secret:

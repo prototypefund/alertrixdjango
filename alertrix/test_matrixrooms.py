f3c2e50447f19a9dd0876224833f38ccc3d204e4
Fabian Becker <info@alertrix.net>
2024-09-12 10:56:32 +0200

    import: BeautifulSoup from bs4 into alertrix/test_matrixrooms.py

--- alertrix/test_matrixrooms.py
+++ alertrix/test_matrixrooms.py
@@ -4,6 +4,7 @@ from urllib.parse import urlencode
 import nio
 import time
 from asgiref.sync import sync_to_async
+from bs4 import BeautifulSoup
 from django.contrib.auth import get_user_model
 from django.test import AsyncClient
 from django.urls import reverse

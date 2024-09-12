aaed18845182eae1eddc7930d07656f9f0e886d3
Fabian Becker <info@alertrix.net>
2024-09-12 10:55:54 +0200

    import: nio into alertrix/test_matrixrooms.py

--- alertrix/test_matrixrooms.py
+++ alertrix/test_matrixrooms.py
@@ -1,6 +1,7 @@
 from unittest import IsolatedAsyncioTestCase
 from urllib.parse import urlencode
 
+import nio
 from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
 from django.test import AsyncClient

6c4da8eed5a78f344db51c58761e64061eb9b234
Fabian Becker <info@alertrix.net>
2024-09-15 18:36:14 +0200

    import: models as matrixappservice from matrixappservice into alertrix/views/alert_channel.py

--- alertrix/views/alert_channel.py
+++ alertrix/views/alert_channel.py
@@ -3,3 +3,4 @@ import nio
 from django.contrib import messages
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from matrixappservice import models as matrixappservice

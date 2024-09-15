9e1188bce6a0f022fc6d4394cd59aefd6c8a55a6
Fabian Becker <info@alertrix.net>
2024-09-15 15:18:52 +0200

    Revert "import: Group from django.contrib.auth.models into views/company.py"
    This reverts commit 8a533d3cf9c84281a2f04044db366f691d842dbd.

--- alertrix/views/company.py
+++ alertrix/views/company.py
@@ -9,7 +9,6 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.contrib.auth.models import Group
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView

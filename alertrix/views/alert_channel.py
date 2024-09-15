ba5c57602d2e8f17300062905c8815d575936a32
Fabian Becker <info@alertrix.net>
2024-09-15 18:37:58 +0200

    import: MatrixError from matrixappservice.exceptions into alertrix/views/alert_channel.py

--- alertrix/views/alert_channel.py
+++ alertrix/views/alert_channel.py
@@ -4,3 +4,4 @@ from django.contrib import messages
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from matrixappservice import models as matrixappservice
+from matrixappservice.exceptions import MatrixError

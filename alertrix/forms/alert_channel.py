81887b41e4355863077bf84760a035656c3e99b6
Fabian Becker <info@alertrix.net>
2024-09-15 17:10:40 +0200

    import: v1 as events from ..events into alertrix/forms/alert_channel.py

--- alertrix/forms/alert_channel.py
+++ alertrix/forms/alert_channel.py
@@ -3,3 +3,4 @@ from django.utils.translation import gettext_lazy as _
 from matrixappservice import models as matrixappservice
 from . import matrixroom
 from .. import models
+from ..events import v1 as events

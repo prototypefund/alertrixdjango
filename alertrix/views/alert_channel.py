f250c5b3633753fb3fcc92e4ee9bf685e58f36a5
Fabian Becker <info@alertrix.net>
2024-09-15 18:41:14 +0200

    style: add empty line between 3rd party and local imports into alertrix/views/alert_channel.py

--- alertrix/views/alert_channel.py
+++ alertrix/views/alert_channel.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from matrixappservice import models as matrixappservice
 from matrixappservice.exceptions import MatrixError
+
 from . import matrixroom
 from .. import forms
 from .. import models

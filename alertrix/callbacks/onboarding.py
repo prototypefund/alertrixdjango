e302a0252192425e1eb94d83a336b2897ff9e9a6
Fabian Becker <info@alertrix.net>
2024-09-16 10:47:14 +0200

    fix: use the correct model and model manager when accessing DirectMessage model in alertrix.callbacks.onboarding.on_user_joined_company

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -162,7 +162,7 @@ async def on_user_joined_company(
         event: nio.RoomMemberEvent,
 ):
     try:
-        await querysets.aget_direct_message_for(
+        await models.DirectMessage.objects.aget_for(
             event.sender,
             client.user_id,
         )

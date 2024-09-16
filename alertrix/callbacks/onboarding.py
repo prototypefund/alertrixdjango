e10620e9c131fbd7665818d224d21a2ebb66efc0
Fabian Becker <info@alertrix.net>
2024-09-16 10:38:49 +0200

    import: gettext_lazy as _ from django.utils.translation into alertrix/callbacks/onboarding.py

--- alertrix/callbacks/onboarding.py
+++ alertrix/callbacks/onboarding.py
@@ -5,6 +5,7 @@ import nio
 from asgiref.sync import sync_to_async
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from matrixappservice import MatrixClient
 from matrixappservice import models as mas_models
 

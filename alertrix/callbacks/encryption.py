5e7f252e84768dfd2b9f495939d63f07e0c44e3a
Fabian Becker <info@alertrix.net>
2024-10-21 13:49:08 +0200

    create logger for alertrix.callbacks.encryption module

--- alertrix/callbacks/encryption.py
+++ alertrix/callbacks/encryption.py
@@ -5,6 +5,17 @@ import nio
 from asgiref.sync import sync_to_async
 from matrixappservice import MatrixClient
 
+logger = logging.getLogger(__name__)
+log_separator = '__'
+log_id_prefix = 'LOG'
+var_name = '%s%s%s' % (log_id_prefix, log_separator, __name__.upper().replace('.', log_separator))
+while True:
+    if os.getenv(var_name) is not None or var_name == 'LOG':
+        break
+    var_name = log_separator.join(var_name.split(log_separator)[:-1])
+logger.setLevel(os.getenv(var_name, '').upper() or logger.getEffectiveLevel())
+logger.addHandler(logging.StreamHandler())
+
 
 async def verify_all_devices(
         client: MatrixClient,
